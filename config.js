5b3f0ce34997d86d9f93df7f62af266038427ebf
liuqingzhi <liuqingzhi@ehousechina.com>
2019-12-02 10:39:38 +0800

    使用我自己的配置

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   version: "7.2.0",
   note: '优化登录授权机制',
-  subDomain: "tz", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
-  appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
+  subDomain: "liuqingzhi", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
+  appid: "wx3c462ed54648794c", // 您的小程序的appid，购物单功能需要使用
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   requireBindMobile: true, // 是否强制绑定手机号码才能使用
   kanjiaRequirePlayAd: true // 是否必须要观看视频广告后才可以砍价
